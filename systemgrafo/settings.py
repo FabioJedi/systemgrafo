df1d1d8cf6ce2bb018c7efe6ebd4dd5543c7604f
Fábio Araujo <fabio.araujo@soulasalle.com.br>
2019-04-23 20:29:43 -0300

    Add linha comentada em STATIC_URL

--- systemgrafo/settings.py
+++ systemgrafo/settings.py
@@ -117,5 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+#Endereço da URL onde se encontram os arquivos statics
+#STATIC_URL = 'http://localhost:8001/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
